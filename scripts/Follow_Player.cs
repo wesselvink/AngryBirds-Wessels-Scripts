2f1e2b989aceccf0caf4ce39c2ffd56d1264e2a0
wesselvink <22092@ma-web.nl>
2018-02-02 16:26:18 +0100

    scripts

---
+++ scripts/Follow_Player.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow_Player : MonoBehaviour {
+
+    public GameObject Sphere;     //Public variable to store a reference to the player game object
+
+	private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+    private float _camPosition;
+
+
+    ProjectileDraggingScript projectiledraggingscript;
+
+    bool DragStatus
+    {
+        get { return PROJECTILEDRAGGINGSCRIPT.BeenShot; }
+        set { PROJECTILEDRAGGINGSCRIPT.BeenShot = value; }
+    }
+
+    public ProjectileDraggingScript PROJECTILEDRAGGINGSCRIPT
+    {
+        get { return projectiledraggingscript ?? (projectiledraggingscript = GetComponent<ProjectileDraggingScript>()); }
+    }
+
+    // Use this for initialization
+    void Start () 
+	{
+        offset = transform.position - Sphere.transform.position;
+        projectiledraggingscript = GameObject.FindGameObjectWithTag("Player").GetComponent<ProjectileDraggingScript>();
+    }
+
+	// LateUpdate is called after Update each frame
+	void Update () 
+	{
+        _camPosition = Sphere.transform.position.x;
+
+            //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        if (projectiledraggingscript.BeenShot)
+        {
+            // Debug.Log("Oh Fuck Off"); //mijn reactie toen het toch wel werkte
+            transform.position = new Vector3(_camPosition, 0) + offset;
+        }
+
+    }
+}
---
+++ scripts/Pause.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Pause : MonoBehaviour {
+	public Transform pause_Canvas;
+	public Transform Buttons;
+	public Transform Player;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			PauseGame ();
+		}
+	}
+
+
+
+	public void PauseGame() {
+		if (pause_Canvas.gameObject.activeInHierarchy == false) {
+			pause_Canvas.gameObject.SetActive (true);
+			Time.timeScale = 0;
+            Player.GetComponent<ProjectileDraggingScript>().enabled = false;
+
+		} else {
+			pause_Canvas.gameObject.SetActive (false);
+			Time.timeScale = 1;
+            Player.GetComponent<ProjectileDraggingScript>().enabled = true;
+
+        }
+    }
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        Player.GetComponent<ProjectileDraggingScript>().enabled = true;
+        SceneManager.LoadScene("AngryScene");
+    }
+}
